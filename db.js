529f3107350370b14606add1db10e20820106a38
Quy Huynh <huynhtranngocquy@gmail.com>
2022-01-09 08:25:19 +0700

    update psql

--- db.js
+++ db.js
@@ -1,11 +1,22 @@
 const Pool = require('pg').Pool;
+require('dotenv').config();
 
+const isProduction = process.env.NODE_ENV === "production";
+
+// const pool = new Pool({
+//     user: "postgres",
+//     host: "localhost",
+//     database: "students",
+//     password: "1234",
+//     port: 5432,
+// })
+
+const connectionString = `postgres://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`;
 const pool = new Pool({
-    user: "postgres",
-    host: "localhost",
-    database: "students",
-    password: "1234",
-    port: 5432,
-})
+    connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
+    ssl: {
+        rejectUnauthorized: false,
+    },
+});
 
 module.exports = pool;
